25937772f6a4a639e4159f7f86bc063a4d2d00ac
DrowLink <pauloriveiro01@gmail.com>
2024-02-23 16:12:01 -0400

    feature: added providers & chat bubble refacts

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: unnecessary_const
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/shared/message_field_box.dart';
@@ -23,7 +26,7 @@ class ChatScreen extends StatelessWidget {
         ),
         title: const Text('My Love'),
       ),
-      body: _ChatView(),
+      body: const _ChatView(),
     );
   }
 }
@@ -35,6 +38,9 @@ class _ChatView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric( horizontal: 10),
@@ -42,16 +48,21 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: 50,
+                controller: chatProvider.chatScrollController,
+                itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
-                  return ( index % 2 == 0 ) //Way to render different bubbles
-                  ? const HerMessageBubble() //HerMessagebubble Widget
-                  : const MyMessageBubble(); //MyMessagebubble Widget
+                  final message = chatProvider.messageList[index];
+
+                  return (message.fromWho == FromWho.hers)
+                  ? const HerMessageBubble()
+                  :  MyMessageBubble( message: message,);
               })
             ),
 
             /// Caja de Texto de mensaje
-            const MessageFieldBox(),
+            MessageFieldBox(
+              onValue: (value) => chatProvider.sendMessage(value),
+            ),
           ],
         ),
       ),
--- lib/presentation/widgets/chat/shared/message_field_box.dart
+++ lib/presentation/widgets/chat/shared/message_field_box.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({
+    super.key,
+    required this.onValue,
+    });
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +28,9 @@ class MessageFieldBox extends StatelessWidget {
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
+            final textValue = textController.value.text;
             textController.clear();
+            onValue( textValue );
           },
         )
       );
@@ -37,6 +45,7 @@ class MessageFieldBox extends StatelessWidget {
       onFieldSubmitted: (value) {
         textController.clear();
         focusNode.requestFocus();
+        onValue(value);
       },
     );
 
